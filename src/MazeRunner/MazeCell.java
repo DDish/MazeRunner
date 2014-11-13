73cccfe4393a0fd7def8b6cf7ce2df8bce6eccf5
txgamingmonster <stunger@mines.edu>
2014-11-13 11:00:50 -0700

    Changed made to Maze.csv, adding NUMROWS and COLS, changes to MazeCell and Maze

--- src/MazeRunner/MazeCell.java
+++ src/MazeRunner/MazeCell.java
@@ -2,4 +2,27 @@ package MazeRunner;
 
 public class MazeCell {
 
+	private int row,col;
+	private char initial;
+	private boolean boo;
+	
+	public MazeCell(int r, int c, boolean b, char mnum) {
+		// TODO Auto-generated constructor stub
+		row = r;
+		col = c;
+		boo = b;
+		initial = mnum;
+	}
+	
+	public char getInitial() {
+		return initial;
+	}
+	
+	public boolean isMarker() {
+		return boo;
+	}
+
+	public void draw() {
+		
+	}
 }
--- src/Tests/mazeTests.java
+++ src/Tests/mazeTests.java
@@ -2,6 +2,8 @@ package Tests;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -13,26 +15,32 @@ public class mazeTests {
 	private static Hub hub;
 	private static Maze maze;
 	private static MazeCell[][] map;
+	private static ArrayList<Character> markers;
 	public static final int NUM_MARKS = 14;
 	public static final int NUM_ROWS = 34;
 	public static final int NUM_COLUMNS = 34;
 	
 	@Before
 	public void setUp() throws Exception {
-		hub = new Hub("MazeLayout.csv");
+		hub = new Hub("Maze.csv");
 		maze = hub.getMaze();
 		map = maze.getMazeMap();
+		markers = maze.getMarkers();
 	}
 
 	@Test
 	public void test() {
-		Assert.assertEquals(NUM_MARKS,maze.getMarkers().size());
 		Assert.assertEquals(NUM_ROWS, map.length);
 		Assert.assertEquals(NUM_COLUMNS, map[0].length);
+		Assert.assertEquals(NUM_MARKS,markers.size());
 		
 		for(int i=0;i<map.length;i++)
 			for(int j=0;j<map[0].length;j++)
+			{
 				Assert.assertNotNull(map[i][j]);
+				Assert.assertTrue(markers.contains(map[i][j].getInitial()));
+			}
+		
 	}
 
 }
