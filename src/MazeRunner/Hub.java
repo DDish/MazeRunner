717a66f6414993e44807c9c79c4473f46de51b81
txgamingmonster <stunger@mines.edu>
2014-11-18 11:37:35 -0700

    Added starting location marker and robot list

--- src/MazeRunner/Hub.java
+++ src/MazeRunner/Hub.java
@@ -1,6 +1,8 @@
 package MazeRunner;
 
+import java.util.LinkedList;
 import java.util.Queue;
+
 import javax.swing.*;
 import javax.swing.border.EtchedBorder;
 
@@ -11,9 +13,27 @@ public class Hub extends JFrame {
 	private Maze maze;
 	private JPanel mazePanel;
 	private Queue<Robot> robots;
+	private JComboBox<Character> box;
+	private JTextArea rlist;
+	private JButton find;
 	
 	public Hub(String file){
 		maze = new Maze(file);
+		robots = new LinkedList<Robot>();
+		for(int i=0;i<10;i++)
+			robots.add(new Robot(15,1));
+		
+		box = new JComboBox<Character>();
+		for (char c: maze.getMarkers())
+			box.addItem(c);
+		
+		rlist = new JTextArea();
+		String s = "";
+		for(int i=1;i<=robots.size();i++)
+		{
+			s+="Robot "+i+"\n";
+		}
+		rlist.setText(s);
 		
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setLocation(500,100);
@@ -28,8 +48,9 @@ public class Hub extends JFrame {
 	}
 	
 	private void createGUI() {
-		add(maze, BorderLayout.CENTER);
-		JPanel p = new JPanel();
+		add(maze, BorderLayout.CENTER);			//MAZE
+		
+		JPanel p = new JPanel();				//LEGEND
 		p.setLayout(new GridLayout(1,2));
 		p.add(new LPanel(this));
 		JTextArea t = new JTextArea();
@@ -37,6 +58,24 @@ public class Hub extends JFrame {
 		p.add(t);
 		add(p, BorderLayout.SOUTH);
 		
+		JPanel r = new JPanel();
+		r.setLayout(new GridLayout(2,1));
+		
+		JPanel r1 = new JPanel();
+		r1.setLayout(new BorderLayout());
+		r1.add(new JLabel("Robot List:"), BorderLayout.NORTH);
+		r1.add(rlist,BorderLayout.CENTER);
+		r.add(r1);
+		
+		JPanel r2 = new JPanel();
+		r2.setLayout(new GridLayout(3,1));
+		r2.add(new JLabel("Robot Destination:"),BorderLayout.NORTH);
+		r2.add(box,BorderLayout.CENTER);
+		find = new JButton("Find");
+		r2.add(find,BorderLayout.SOUTH);
+		r.add(r2);
+		
+		add(r, BorderLayout.EAST);
 	}
 	
 	public static void main(String[] args) {
