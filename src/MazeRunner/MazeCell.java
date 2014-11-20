46694597128916e856a5c52a476a80368d1830f5
unknown <janelson@MZ022-42.adit.mines.edu>
2014-11-20 12:01:06 -0700

    updated mazeCells to draw breadCrumbs when called.

--- src/MazeRunner/MazeCell.java
+++ src/MazeRunner/MazeCell.java
@@ -4,11 +4,14 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.util.ArrayList;
 
+import sun.awt.RepaintArea;
+
 public class MazeCell {
 	private int row,col,r1=0,g1=0,b1=0;
 	private char initial;
 	private boolean isMarker;
 	private Color color;
+	private boolean hasBreadcrumb;
 	
 	public MazeCell(int r, int c, boolean b, char mnum) {
 		// TODO Auto-generated constructor stub
@@ -48,9 +51,21 @@ public class MazeCell {
 	public boolean isMarker() {
 		return isMarker;
 	}
+	
+	public void dropBreadcrumb(){
+		hasBreadcrumb = true;
+	}
+	public void pickupBreadCrumb(){
+		hasBreadcrumb = false;
+	}
+	
 
 	public void draw(Graphics g, int x, int y, Color c) {
 		g.setColor(c);
 		g.fillRect(40+x*16,40+y*16,15,15);
+		if(hasBreadcrumb){
+			g.setColor(Color.LIGHT_GRAY);
+			g.fillOval(40+x*16,40+y*16,15,15);
+		}
 	}
 }
